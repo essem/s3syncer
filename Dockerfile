f74805cb3f785770681c63008083477da7bed91f
essem <soomin@gmail.com>
2020-10-27 16:12:59 +0900

    aws cli를 사용하여 s3 sync를 수행

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,15 @@
 FROM node:12-alpine
+
 WORKDIR /app
+
+# awscli
+RUN apk add --no-cache python3 py3-pip
+RUN pip3 install --upgrade pip
+RUN pip3 install awscli
+RUN rm -rf /var/cache/apk/*
+RUN aws --version
+
+# app
 ENV NODE_ENV production
 COPY package.json yarn.lock ./
 RUN yarn install --frozen-lockfile --production
--- src/index.js
+++ src/index.js
@@ -1,36 +1,56 @@
+const { spawnSync } = require("child_process");
+
+const source = "/logs";
+const target = "s3://mybucket/";
+const syncInterval = 3;
+const shutdownDelay = 3;
+
 function sleep(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
 
-let shutdown = false;
+function sync() {
+  const cmd = `aws s3 sync ${source} ${target}`;
+  console.log(cmd);
+  const child = spawnSync(cmd, {
+    stdio: "inherit",
+    shell: true,
+  });
+}
+
+let inShutdown = false;
 
 async function main() {
-  while (!shutdown) {
-    console.log(new Date());
-    await sleep(1000);
+  while (!inShutdown) {
+    sync();
+
+    console.log(`sleep for ${syncInterval} seconds`);
+    await sleep(syncInterval * 1000);
   }
 }
 
-async function cleanup() {
-  if (shutdown) {
+async function shutdown() {
+  if (inShutdown) {
     console.log("already in shutdown");
     return;
   }
 
-  shutdown = true;
+  inShutdown = true;
   console.log("start shutdown...");
 
-  for (let i = 5; i > 0; i--) {
+  for (let i = shutdownDelay; i > 0; i--) {
     console.log(i);
     await sleep(1000);
   }
 
-  console.log("exit");
+  sync();
+
+  console.log("goodbye");
   process.exit(0);
 }
 
-process.on("SIGINT", () => cleanup());
-process.on("SIGTERM", () => cleanup());
+process.on("SIGINT", () => shutdown());
+process.on("SIGTERM", () => shutdown());
 main();
